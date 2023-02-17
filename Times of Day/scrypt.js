29cc75d25c48cf20b98e8b280d0754d14c868694
juan <99427234+juan9321@users.noreply.github.com>
2023-02-16 22:19:58 -0300

    Times of Day

---
+++ Times of Day/scrypt.js
@@ -0,0 +1,11 @@
+function carregar() {
+
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `agora sao ${hora} horas. `
+
+}
+
+
